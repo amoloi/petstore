aac641f1de6a13927416fcf3cf1a2e25a5f2af01
Dominik Zogg <dominik.zogg@gmail.com>
2019-07-16 22:40:55 +0200

    Controller => RequestHandler

--- app/web.php
+++ app/web.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace App;
 
-use App\Controller\Crud\CreateController;
-use App\Controller\Crud\DeleteController;
-use App\Controller\Crud\ListController;
-use App\Controller\Crud\ReadController;
-use App\Controller\Crud\UpdateController;
-use App\Controller\IndexController;
-use App\Controller\PingController;
-use App\Controller\Swagger\IndexController as SwaggerIndexController;
-use App\Controller\Swagger\YamlController as SwaggerYamlController;
 use App\Model\Pet;
-use App\ServiceProvider\ControllerServiceProvider;
+use App\RequestHandler\Crud\CreateRequestHandler;
+use App\RequestHandler\Crud\DeleteRequestHandler;
+use App\RequestHandler\Crud\ListRequestHandler;
+use App\RequestHandler\Crud\ReadRequestHandler;
+use App\RequestHandler\Crud\UpdateRequestHandler;
+use App\RequestHandler\IndexRequestHandler;
+use App\RequestHandler\PingRequestHandler;
+use App\RequestHandler\Swagger\IndexRequestHandler as SwaggerIndexRequestHandler;
+use App\RequestHandler\Swagger\YamlRequestHandler as SwaggerYamlRequestHandler;
 use App\ServiceProvider\MiddlewareServiceProvider;
+use App\ServiceProvider\RequestHandlerServiceProvider;
 use App\ServiceProvider\ZendExpressiveServiceProvider;
 use Chubbyphp\ApiHttp\Middleware\AcceptAndContentTypeMiddleware;
 use Pimple\Container;
@@ -34,8 +34,8 @@ require __DIR__.'/bootstrap.php';
 
 /** @var Container $container */
 $container = require __DIR__.'/container.php';
-$container->register(new ControllerServiceProvider());
 $container->register(new MiddlewareServiceProvider());
+$container->register(new RequestHandlerServiceProvider());
 $container->register(new ZendExpressiveServiceProvider());
 
 $web = new Application(
@@ -51,14 +51,22 @@ $web->pipe(MethodNotAllowedMiddleware::class);
 $web->pipe(DispatchMiddleware::class);
 $web->pipe(NotFoundHandler::class);
 
-$web->get('/', IndexController::class, 'index');
-$web->get('/api', SwaggerIndexController::class, 'swagger_index');
-$web->get('/api/swagger', SwaggerYamlController::class, 'swagger_yml');
-$web->get('/api/ping', [AcceptAndContentTypeMiddleware::class, PingController::class], 'ping');
-$web->get('/api/pets', [AcceptAndContentTypeMiddleware::class, ListController::class.Pet::class], 'pet_list');
-$web->post('/api/pets', [AcceptAndContentTypeMiddleware::class, CreateController::class.Pet::class], 'pet_create');
-$web->get('/api/pets/{id}', [AcceptAndContentTypeMiddleware::class, ReadController::class.Pet::class], 'pet_read');
-$web->put('/api/pets/{id}', [AcceptAndContentTypeMiddleware::class, UpdateController::class.Pet::class], 'pet_update');
-$web->delete('/api/pets/{id}', [AcceptAndContentTypeMiddleware::class, DeleteController::class.Pet::class], 'pet_delete');
+$web->get('/', IndexRequestHandler::class, 'index');
+$web->get('/api', SwaggerIndexRequestHandler::class, 'swagger_index');
+$web->get('/api/swagger', SwaggerYamlRequestHandler::class, 'swagger_yml');
+$web->get('/api/ping', [AcceptAndContentTypeMiddleware::class, PingRequestHandler::class], 'ping');
+$web->get('/api/pets', [AcceptAndContentTypeMiddleware::class, ListRequestHandler::class.Pet::class], 'pet_list');
+$web->post('/api/pets', [AcceptAndContentTypeMiddleware::class, CreateRequestHandler::class.Pet::class], 'pet_create');
+$web->get('/api/pets/{id}', [AcceptAndContentTypeMiddleware::class, ReadRequestHandler::class.Pet::class], 'pet_read');
+$web->put(
+    '/api/pets/{id}',
+    [AcceptAndContentTypeMiddleware::class, UpdateRequestHandler::class.Pet::class],
+    'pet_update'
+);
+$web->delete(
+    '/api/pets/{id}',
+    [AcceptAndContentTypeMiddleware::class, DeleteRequestHandler::class.Pet::class],
+    'pet_delete'
+);
 
 return $web;
