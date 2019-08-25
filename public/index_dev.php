b7a8fcdd1cfd14fe91ba41142340873dfe325f15
Dominik Zogg <dominik.zogg@gmail.com>
2019-08-25 12:25:29 +0200

    put vdoc at the correct position

--- public/index_dev.php
+++ public/index_dev.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 use Slim\Psr7\Factory\ServerRequestFactory;
 
-/** @var Slim\App $web */
-
 $env = 'dev';
 
+/** @var Slim\App $web */
 $web = require __DIR__ . '/../app/web.php';
 $web->run((new ServerRequestFactory())->createFromGlobals());
--- public/index_prod.php
+++ public/index_prod.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 use Slim\Psr7\Factory\ServerRequestFactory;
 
-/** @var Slim\App $web */
-
 $env = 'prod';
 
+/** @var Slim\App $web */
 $web = require __DIR__ . '/../app/web.php';
 $web->run((new ServerRequestFactory())->createFromGlobals());
