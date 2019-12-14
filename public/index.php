d73fe081eff7c4d5f8555507d70262629ed5045b
Dominik Zogg <dominik.zogg@gmail.com>
2019-12-14 14:20:58 +0100

    fix environment variable handling

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@
 declare(strict_types=1);
 
 /** @var Zend\Expressive\Application $web */
-$web = (require __DIR__ . '/../app/web.php')($_ENV['APP_ENV']);
+$web = (require __DIR__ . '/../app/web.php')(getenv('APP_ENV'));
 $web->run();
