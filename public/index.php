cb43d57f0abb1409cd5612e566ffbd31d2cfdcda
Dominik Zogg <dominik.zogg@gmail.com>
2019-12-14 14:20:58 +0100

    fix environment variable handling

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use Slim\Psr7\Factory\ServerRequestFactory;
 
 /** @var Slim\App $web */
-$web = (require __DIR__ . '/../app/web.php')($_ENV['APP_ENV']);
+$web = (require __DIR__ . '/../app/web.php')(getenv('APP_ENV'));
 $web->run((new ServerRequestFactory())->createFromGlobals());
