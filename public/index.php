dba4ed199cce676b9bd0aa19e6316422259e3659
Dominik Zogg <dominik.zogg@gmail.com>
2019-12-14 14:14:36 +0100

    use enviroment variables

--- public/index.php
+++ public/index.php
@@ -1,1 +1,7 @@
-index_dev.php
+<?php
+
+declare(strict_types=1);
+
+/** @var Zend\Expressive\Application $web */
+$web = (require __DIR__ . '/../app/web.php')($_ENV['APP_ENV']);
+$web->run();
--- public/index_dev.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-/** @var Zend\Expressive\Application $web */
-$web = (require __DIR__ . '/../app/web.php')('dev');
-$web->run();
